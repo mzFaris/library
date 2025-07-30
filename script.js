363d0a87f686780b400b3252f56a965dc0a06380
Faris <alarifinmirzafaris@gmail.com>
2025-07-30 11:52:31 +0700

    Initialize basic library project structure

---
+++ script.js
@@ -0,0 +1,10 @@
+const myLibrary = [];
+
+function Book(title, author, page, read) {
+  this.title = title;
+  this.author = author;
+  this.page = page;
+  this.read = read;
+}
+
+function addBookToLibrary() {}
