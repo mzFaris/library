6c424470ffbf132d293d8bfa28fa4b6770963b35
Faris <alarifinmirzafaris@gmail.com>
2025-07-31 12:10:38 +0700

    Add unique ID field to Book constructor

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const myLibrary = [];
 
-function Book(title, author, page, read) {
+function Book(id, title, author, page, read) {
+  this.id = id;
   this.title = title;
   this.author = author;
   this.page = page;
   this.read = read;
 }
 
-function addBookToLibrary() {}
+function addBookToLibrary(title, author, page, read) {
+  const book = new Book(crypto.randomUUID(), title, author, page, read);
+  myLibrary.push(book);
+}
