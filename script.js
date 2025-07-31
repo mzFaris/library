679edc6a6266ac9fd325bb7293c4046a221a31ef
Faris <alarifinmirzafaris@gmail.com>
2025-07-31 13:10:44 +0700

    Add book card DOM manipulation and sample data

--- script.js
+++ script.js
@@ -12,3 +12,40 @@ function addBookToLibrary(title, author, page, read) {
   const book = new Book(crypto.randomUUID(), title, author, page, read);
   myLibrary.push(book);
 }
+
+function addCardToDOM(book) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const title = document.createElement("p");
+  title.textContent = `Title: ${book.title}`;
+  const author = document.createElement("p");
+  author.textContent = `Author: ${book.author}`;
+  const page = document.createElement("p");
+  page.textContent = `Pages: ${book.page}`;
+  const read = document.createElement("p");
+  read.textContent = `Status: ${book.read}`;
+
+  card.append(title, author, page, read);
+
+  content.appendChild(card);
+}
+
+addBookToLibrary(
+  "Omniscient Reader's Viewpoint",
+  "Sing Shong",
+  498,
+  "Completed",
+);
+addBookToLibrary(
+  "Harry Potter and the Deathly Hallows",
+  "J. K. Rowling",
+  607,
+  "Reading",
+);
+
+const content = document.querySelector(".content");
+
+for (const book of myLibrary) {
+  addCardToDOM(book);
+}
