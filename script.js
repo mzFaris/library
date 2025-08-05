a3a2fbd0f4c9051a869d9c4eac5ccf472ac29a7e
Faris <alarifinmirzafaris@gmail.com>
2025-08-05 14:07:45 +0700

    implement function to display book

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ function addBookToLibrary(title, author, pages, read) {
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
 }
+
+const cardContainer = document.querySelector("#card-container");
+
+function displayBooks() {
+  myLibrary.forEach((book) => {
+    const card = document.createElement("div");
+
+    for (const prop in book) {
+      const p = document.createElement("p");
+      p.textContent = `${prop}: ${book[prop]}`;
+      card.append(p);
+    }
+
+    cardContainer.append(card);
+  });
+}
+
+addBookToLibrary("Omniscient Reader's Viewpoint", "Sing Shong", 500, true);
+addBookToLibrary("Lookism", "Taejun Pak", 300, false);
+
+displayBooks();
