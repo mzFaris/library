c8925ebf87dfc345ea10a202fa58bc39c64f80ff
Mirza Faris Al Arifin <alarifinmirzafaris@gmail.com>
2025-08-17 09:36:21 +0700

    implement display book to DOM

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const content = document.querySelector("#content");
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -12,3 +13,32 @@ function addBookToLibrary(title, author, pages, read) {
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
 }
+
+function displayBook() {
+  myLibrary.forEach((book) => {
+    const card = document.createElement("div");
+
+    const title = document.createElement("p");
+    const author = document.createElement("p");
+    const pages = document.createElement("p");
+    const read = document.createElement("p");
+    const id = document.createElement("p");
+
+    title.textContent = `Title: ${book.title}`;
+    author.textContent = `Author: ${book.author}`;
+    pages.textContent = `Pages: ${book.pages}`;
+    read.textContent = `Read: ${book.read}`;
+    id.textContent = `Id: ${book.id}`;
+
+    card.append(title, author, pages, read, id);
+
+    content.append(card);
+  });
+}
+
+addBookToLibrary("asdsdf", "sadfsdf", 5, true);
+addBookToLibrary("asdsdf", "sadfsdf", 5, true);
+addBookToLibrary("asdsdf", "sadfsdf", 5, true);
+addBookToLibrary("asdsdf", "sadfsdf", 5, true);
+
+displayBook();
